fc8b4af92b2bb795642b6ebb0b054e8144e66b81
Fatma NABLI <nabli.isigk@gmail.com>
2023-07-24 12:26:59 +0200

    Corrections liens avec link

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -10,17 +10,17 @@ function SignIn(){
               <input type="text" id="username" />
             </div>
             <div className="input-wrapper">
-              <label htmlFor="password">Password</label
-              ><input type="password" id="password" />
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
             </div>
             <div className="input-remember">
               <input type="checkbox" id="remember-me" />
               <label htmlFor="remember-me">Remember me</label>
             </div>
             {/* PLACEHOLDER DUE TO STATIC SITE  */}
-            <a href="./user.html" className="sign-in-button">Sign In</a>
+            {/* <a href="./user.html" className="sign-in-button">Sign In</a> */}
             {/* SHOULD BE THE BUTTON BELOW */}
-            {/* <button className="sign-in-button">Sign In</button>  */}
+            <button className="sign-in-button">Sign In</button> 
            
           </form>
         </section>
